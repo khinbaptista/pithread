f0732110b0af741ba06643b91959009698dcaa06
khin <khin.baptista@gmail.com>
2015-09-23 20:14:51 -0300

    Found issue with piyield.
    Apparently, it does not come back to where it is supposed to. It may be calling the thread from the entry point... It is definetely a context saving issue.

--- testes/teste1.c
+++ testes/teste1.c
@@ -1,16 +1,44 @@
-#include <stdlib.h>
+//#include <stdlib.h>
 #include <stdio.h>
 #include "pithread.h"
 
-void func1();
-void func2();
-
-int main(int argc, char* argv[]){
+void* func1(void *arg){
+	int i = 0;
+	printf("Index initialized with 0\n");
 	
+	for (; i < 10; i++){
+		printf("thread 1 with index %d\n\n", i);
+
+		if (i % 5 == 0) piyield();
+	}
 	
 	return 0;
 }
 
-void func1(){
+void* func2(void* arg){
+	int i = 0;
 	
+	for (; i < 10; i++){
+		printf("thread 2\n\n");
+		
+		//if (i % 5 == 0) piyield();
+	}
+	
+	return 0;
+}
+
+int main(int argc, char* argv[]){
+	
+	printf("Create new thread.\n");
+	int t1, t2;
+	
+	t1 = picreate(1, func1, 0);
+	t2 = picreate(1, func2, 0);
+	
+	printf("Threads created. Waiting...\n");
+	piwait(t1);
+	piwait(t2);
+	
+	printf("Threads finished. Exiting...\n");
+	return 0;
 }
